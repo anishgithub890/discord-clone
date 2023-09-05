9e642366f5929244ff9b895d00e48833df3589d3
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-09-05 19:38:35 +0545

    shadcn-button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <>
-      <div className="text-2xl text-blue-600">Discord Clone</div>
-    </>
+    <div className="flex flex-col">
+      <p className="text-3xl font-bold text-indigo-500">Hello Discord Clone</p>
+
+      <Button variant="default">Add Me</Button>
+    </div>
   );
 }
