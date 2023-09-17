a8aa175bb311096034e26ed4cd1173cbb3a6e515
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-09-17 15:35:10 +0545

    hooks:use-chat-query.ts

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { ChatHeader } from '@/components/chat/chat-header';
 import { ChatInput } from '@/components/chat/chat-input';
+import { ChatMessages } from '@/components/chat/chat-messages';
 
 interface ChannelIdPageProps {
   params: {
@@ -44,7 +45,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         type="channel"
         name={channel.name}
       />
-      <div className="flex-1">Future Messages</div>
+      <ChatMessages />
       <ChatInput
         name={channel.name}
         type="channel"
