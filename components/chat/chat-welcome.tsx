1ee46b69a13d9782cfb15558c88fc2a7391aed49
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-09-17 21:34:50 +0545

    query-provider:from-cmp-provider

--- components/chat/chat-welcome.tsx
+++ components/chat/chat-welcome.tsx
@@ -9,12 +9,12 @@ export const ChatWelcome = ({ name, type }: ChatWelcomeProps) => {
   return (
     <div className="space-y-2 px-4 mb-4">
       {type === 'channel' && (
-        <div className="h-[75px] rounded-full bg-zinc-500 dark:bg-zinc-700 flex items-center justify-center">
+        <div className="h-[75px] w-[75px] rounded-full bg-zinc-500 dark:bg-zinc-700 flex items-center justify-center">
           <Hash className="h-12 w-12 text-white" />
         </div>
       )}
       <p className="text-xl md:text-3xl font-bold">
-        {type === 'channel' ? 'welcome to #' : ''}
+        {type === 'channel' ? 'Welcome to #' : ''}
         {name}
       </p>
       <p className="text-zinc-600 dark:text-zinc-400 text-sm">
