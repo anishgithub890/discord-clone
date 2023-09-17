deb98a1635b4fc79a5bc7e857e4dee4cee8741e7
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-09-17 21:21:36 +0545

    chat-messages,app-channel:-page:ongoing,chat-wlc

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,5 +1,35 @@
 'use client';
 
-export const ChatMessages = () => {
-  return <div>Chat-Messages</div>;
+import { Member } from '@prisma/client';
+import { ChatWelcome } from '@/components/chat/chat-welcome';
+
+interface ChatMessagesProps {
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: 'channelId' | 'conversationId';
+  paramValue: string;
+  type: 'channel' | 'conversation';
+}
+
+export const ChatMessages = ({
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}: ChatMessagesProps) => {
+  return (
+    <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+      <div className="flex-1" />
+      <ChatWelcome name={name} type={type} />
+    </div>
+  );
 };
